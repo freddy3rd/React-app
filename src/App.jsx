c088ba32d2fb759a1fb8942754e4f2f01711f05c
Joseph Benipayo <clifford.dle.arnejo@gmail.com>
2023-05-30 15:25:09 +0800

    update_30/05/23-FrameMotion

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,23 @@ import { useState } from 'react'
 
 import './App.css'
 import MainContent from './components/Main-content'
+import { Route, Routes } from 'react-router'
+import SuccessDisplay from './components/success-display'
+import { AnimatePresence } from 'framer-motion'
 function App() {
 
   return (
     <>
-    <MainContent/>
+  <AnimatePresence>
+
+     <Routes>
+      <Route path='/' element={<MainContent/>}>
+        <Route path='/home' element={<MainContent/>}></Route>
+        <Route path='/success-display' element={<SuccessDisplay/>}></Route>
+      </Route>
+    </Routes>
+
+  </AnimatePresence>
     </>
   )
 }
--- src/components/Main-content.jsx
+++ src/components/Main-content.jsx
@@ -2,20 +2,22 @@ import React from "react"
 import Content from "./content";
 import SuccessDisplay from "./success-display";
 import { useState } from "react";
+import { AnimatePresence } from "framer-motion";
 
 export default function MainContent(){
     const [email, setEmail] = useState("")
-  
+    
     function useEmail(email){
         setEmail(email)
     }
     return(
         <>
            <div className="container-fluid flex gap-5 justify-center items-center h-[100vh] sm:px-3">
-               
+           <AnimatePresence>
                     {
                        (email !== "") ? <SuccessDisplay value={email} /> :  <Content value={useEmail}/> 
                     }
+            </AnimatePresence>        
            </div>
         </>
     )
